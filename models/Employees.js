4c6c0e393619862ff62a25dc9a54bbc10bf2401c
BoognishSaves <johnpaulhaddad@hotmail.com>
2023-03-10 08:18:47 -0700

    Employee Schema created

---
+++ models/Employees.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+
+const employeesSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, "Please provide a name"]
+        },
+        image: {
+            type: String,
+            required: [true, "Please provide the URL for an image"]
+        },
+        role: {
+            type: String,
+            default: "Grunt"
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+
+const Employees = mongoose.model('Employee', employeesSchema);
+
+module.exports = Employees;
